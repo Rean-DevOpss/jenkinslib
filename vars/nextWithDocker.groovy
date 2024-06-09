c032973b07a2dd4a18fe1f68fdd5658c0c18113b
Muyleang Ing <ing.muyleang2019@rupp.edu.kh>
2024-06-09 17:00:13 +0700

    update

--- vars/nextWithDocker.groovy
+++ vars/nextWithDocker.groovy
@@ -1,4 +1,4 @@
-def call( String TAGS) {
+def call( Integer TAGS) {
     def dockerfileContent = libraryResource 'next.dockerfile'
     writeFile file: 'Dockerfile', text: dockerfileContent
     sh 'docker build -t nexttest:$TAGS .'
