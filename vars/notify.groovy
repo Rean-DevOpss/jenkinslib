f97d1392159e139b07db7768bc8dedc4a009cd66
Muyleang Ing <ing.muyleang2019@rupp.edu.kh>
2024-06-11 11:59:39 +0700

    autopush

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,44 +1,3 @@
-// import org.devops.Notification
-
-// def call(Map params) {
-//     def dockerfileContent = libraryResource 'docker/angular.dockerfile'
-//     writeFile file: 'angular.dockerfile', text: dockerfileContent
-//     try {
-//         withCredentials([usernamePassword(credentialsId: env.REGISTRY_CREDENTIALS_ID, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-//             // docker build
-//             def imageFull = dockerBuild(username: USERNAME, 
-//                 imageName: env.IMGAENAME, 
-//                 tag: env.TAG,
-//                 registryName: env.REGISTRY_NAME    
-//             )
-//             // docker push
-//             if(env.REGISTRY_NAME == 'docker.io'){
-//                 sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-//                 sh "docker push ${imageFull}"
-//             }else{
-//                 sh "docker login -u ${USERNAME} -p ${PASSWORD} ${env.REGISTRY_NAME}"
-//                 sh "docker push ${imageFull}"
-//             }
-//         }
-//     } catch (Exception e) {
-//         def notify = new Notification(steps, this)
-//         notify.sendTelegram("Build failed⛔(<:>) Error: ${e.getMessage()}")
-//         echo "Build failed⛔(<:>) Error: ${e.getMessage()}"
-//         currentBuild.result = 'FAILURE'
-//         throw e
-//     }
-// }
-// def String dockerBuild(Map params){
-//     def dockerImage = "${params.username}/${params.IMGAENAME}:${params.TAG}"
-//     if(env.REGISTRY_NAME != 'docker.io'){
-//         dockerImage = "${env.REGISTRY_NAME}/${params.IMGAENAME}:${params.TAG}"
-//     }
-//     sh "docker build -t ${dockerImage} -f angular.dockerfile ."
-//     def notify = new Notification(steps, this)
-//     notify.sendTelegram("Build success✅(<:>) Image: ${dockerImage}")
-//     return dockerImage
-// }
-
 import org.devops.Notification
 
 def call(Map params) {
@@ -60,6 +19,10 @@ def call(Map params) {
                 sh "docker login -u ${USERNAME} -p ${PASSWORD} ${env.REGISTRY_NAME}"
             }
             sh "docker push ${imageFull}"
+
+            // If everything above is successful, send a success notification
+            def notify = new Notification(steps, this)
+            notify.sendTelegram("Build success✅(<:>) Image: ${imageFull}")
         }
     } catch (Exception e) {
         def notify = new Notification(steps, this)
@@ -73,11 +36,49 @@ def call(Map params) {
 def String dockerBuild(Map params) {
     def dockerImage = "${params.registryName == 'docker.io' ? '' : params.registryName + '/'}${params.username}/${params.imageName}:${params.tag}"
     sh "docker build -t ${dockerImage} -f angular.dockerfile ."
-    def notify = new Notification(steps, this)
-    notify.sendTelegram("Build success✅(<:>) Image: ${dockerImage}")
-    sh "echo ${dockerImage}"
-    // docker rm $(docker ps -a -q)
-    // docker rmi $(docker images -q)
-    sh "docker rmi -f ${dockerImage}"
     return dockerImage
 }
+
+
+// import org.devops.Notification
+
+// def call(Map params) {
+//     def dockerfileContent = libraryResource 'docker/angular.dockerfile'
+//     writeFile file: 'angular.dockerfile', text: dockerfileContent
+
+//     try {
+//         withCredentials([usernamePassword(credentialsId: env.REGISTRY_CREDENTIALS_ID, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+//             // docker build
+//             def imageFull = dockerBuild(username: USERNAME, 
+//                                         imageName: env.IMAGENAME, 
+//                                         tag: env.TAG, 
+//                                         registryName: env.REGISTRY_NAME)
+            
+//             // docker push
+//             if (env.REGISTRY_NAME == 'docker.io') {
+//                 sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+//             } else {
+//                 sh "docker login -u ${USERNAME} -p ${PASSWORD} ${env.REGISTRY_NAME}"
+//             }
+//             sh "docker push ${imageFull}"
+//         }
+//     } catch (Exception e) {
+//         def notify = new Notification(steps, this)
+//         notify.sendTelegram("Build failed⛔(<:>) Error: ${e.getMessage()}")
+//         echo "Build failed⛔(<:>) Error: ${e.getMessage()}"
+//         currentBuild.result = 'FAILURE'
+//         throw e
+//     }
+// }
+
+// def String dockerBuild(Map params) {
+//     def dockerImage = "${params.registryName == 'docker.io' ? '' : params.registryName + '/'}${params.username}/${params.imageName}:${params.tag}"
+//     sh "docker build -t ${dockerImage} -f angular.dockerfile ."
+//     def notify = new Notification(steps, this)
+//     notify.sendTelegram("Build success✅(<:>) Image: ${dockerImage}")
+//     sh "echo ${dockerImage}"
+//     // docker rm $(docker ps -a -q)
+//     // docker rmi $(docker images -q)
+//     sh "docker rmi -f ${dockerImage}"
+//     return dockerImage
+// }
