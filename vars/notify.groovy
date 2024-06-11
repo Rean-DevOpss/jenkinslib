d6f647d7543a0fd2bb6da04e0f7e4dfeddb9ea57
Muyleang Ing <ing.muyleang2019@rupp.edu.kh>
2024-06-11 11:34:41 +0700

    autopush

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,11 +1,36 @@
 import org.devops.Notification
 
 def call(Map params) {
-    def notification = new Notification(steps, this)
-
     try {
-        notification.sendTelegram(params.message)
+        withCredentials([usernamePassword(credentialsId: env.REGISTRY_CREDENTIALS_ID, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+            // docker build
+            def imageFull = dockerBuild(username: USERNAME, 
+                imageName: params.imageName, 
+                tag: params.tag,
+                registryName: env.REGISTRY_NAME    
+            )
+            // docker push
+            if(env.REGISTRY_NAME == 'docker.io'){
+                sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+                sh "docker push ${imageFull}"
+            }else{
+                sh "docker login -u ${USERNAME} -p ${PASSWORD} ${env.REGISTRY_NAME}"
+                sh "docker push ${imageFull}"
+            }
+        }
     } catch (Exception e) {
-        notification.sendTelegram("Failed to send initial message. Error: ${e.getMessage()}")
+        def notify = new Notification(steps, this)
+        notify.sendTelegram("Build failed⛔(<:>) Error: ${e.getMessage()}")
+        echo "Build failed⛔(<:>) Error: ${e.getMessage()}"
+        currentBuild.result = 'FAILURE'
+        throw e
     }
+}
+def String dockerBuild(Map params){
+    def dockerImage = "${params.username}/${params.imageName}:${params.tag}"
+    if(env.REGISTRY_NAME != 'docker.io'){
+        dockerImage = "${env.REGISTRY_NAME}/${params.imageName}:${params.tag}"
+    }
+    sh "docker build -t ${dockerImage} ."
+    return dockerImage
 }
